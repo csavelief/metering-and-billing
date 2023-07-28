b1b6ed3946bae37af29f3007574bd003aa287736
csavelief <csavelief@mncc.fr>
2023-07-28 14:17:14 +0200

    First Commit

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+export class App {
+
+  constructor() {
+  }
+
+  /**
+   * Display the app.
+   *
+   * @param {string} elementId the parent element id.
+   */
+  init(elementId) {
+
+    const container = document.getElementById(elementId);
+    container.innerHTML = `
+      <style>
+        #app-body {
+          background-color: red;
+        }
+      </style>
+      <div id="app-body">
+        <!-- FILLED DYNAMICALLY -->
+      </div>
+    `;
+  }
+}
