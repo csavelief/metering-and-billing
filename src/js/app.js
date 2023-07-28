96ea945847359aae5bcd5f1763176f4ed96dbf6b
csavelief <csavelief@mncc.fr>
2023-07-28 20:37:28 +0200

    Toy pricer.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import {Customers, CustomerSchedule, Events, Features, Plans, Pricer, Prices, Strategies} from "./datastore.js";
+
 export class App {
 
   constructor() {
@@ -48,5 +50,42 @@ export class App {
         </div>
       </div>
     `;
+
+    const customers = new Customers();
+    customers.add('jdoe@example.com');
+
+    const features = new Features();
+    features.addOrUpdate('nb_connections');
+
+    const prices = new Prices();
+    prices.addOrUpdate('1 cent', 'nb_connections', 1);
+
+    const strategies = new Strategies();
+    strategies.addOrUpdate('sum', (events, price) => events.reduce((prev, cur) => prev + (cur.amount * price), 0));
+    strategies.addOrUpdate('mul', (events, price) => events.reduce((prev, cur) => prev * (cur.amount * price), 1));
+
+    const plans = new Plans();
+    plans.addOrUpdate('Plan n°1', 'sum', '1 cent', null, null);
+    plans.addOrUpdate('Plan n°2', 'mul', '1 cent', null, null);
+
+    const schedule = new CustomerSchedule('jdoe@example.com');
+    schedule.add('Plan n°1');
+    schedule.add('Plan n°2');
+
+    const events = new Events();
+    events.add('jdoe@example.com', 'nb_connections', 1);
+    events.add('jdoe@example.com', 'nb_connections', 1);
+    events.add('jdoe@example.com', 'nb_connections', 1);
+
+    const pricer = new Pricer(customers, features, prices, strategies, plans, [schedule], events);
+    const sum1 = pricer.price('jdoe@example.com');
+    const sum2 = pricer.price('jdoe@example.com', ['Plan n°2']);
+    const sum3 = pricer.price('jdoe@example.com', ['Plan n°1']);
+    const sum4 = pricer.price('jdoe@example.com', ['Plan n°1', 'Plan n°2']);
+
+    console.log('[S1] sum price is ' + sum1 + ' (expected=4)');
+    console.log('[S2] sum price is ' + sum2 + ' (expected=1)');
+    console.log('[S3] sum price is ' + sum3 + ' (expected=3)');
+    console.log('[S4] sum price is ' + sum4 + ' (expected=4)');
   }
 }
---
+++ src/js/datastore.js
@@ -0,0 +1,280 @@
+'use strict'
+
+/**
+ * A list of customers.
+ */
+export class Customers {
+
+  constructor() {
+    this.customers_ = [];
+  }
+
+  get(email) {
+    return this.customers_.find(c => c.email === email);
+  }
+
+  add(email) {
+
+    let customer = this.get(email);
+
+    if (customer) {
+      return customer;
+    }
+
+    customer = {
+      id: this.customers_.length + 1, email: email,
+    };
+
+    this.customers_.push(customer);
+    return customer;
+  }
+}
+
+/**
+ * A list of features that can be taken into account in our pricing model.
+ */
+export class Features {
+
+  constructor() {
+    this.features_ = [];
+  }
+
+  get(name) {
+    return this.features_.find(f => f.name === name);
+  }
+
+  addOrUpdate(name) {
+
+    let feature = this.get(name);
+
+    if (feature) {
+      return feature;
+    }
+
+    feature = {
+      id: this.features_.length + 1, name: name
+    };
+
+    this.features_.push(feature);
+    return feature;
+  }
+}
+
+/**
+ * A list of prices associated to each feature.
+ */
+export class Prices {
+
+  constructor() {
+    this.prices_ = [];
+  }
+
+  get(name) {
+    return this.prices_.find(p => p.name === name);
+  }
+
+  addOrUpdate(name, featureName, price) {
+
+    let pricee = this.get(name);
+
+    if (pricee) {
+      pricee.name = name;
+      pricee.feature = featureName;
+      pricee.price = price;
+      return pricee;
+    }
+
+    pricee = {
+      id: this.prices_.length + 1, name: name, feature: featureName, price: price
+    };
+
+    this.prices_.push(pricee);
+    return pricee;
+  }
+}
+
+/**
+ * A strategy applies a given price to a given feature.
+ */
+export class Strategies {
+
+  constructor() {
+    this.strategies_ = [];
+  }
+
+  get(name) {
+    return this.strategies_.find(p => p.name === name);
+  }
+
+  // fnStrategy(the app-generated events for the given feature, the feature's price)
+  addOrUpdate(name, fnStrategy) {
+
+    let strategy = this.get(name);
+
+    if (strategy) {
+      strategy.name = name;
+      strategy.apply = fnStrategy;
+      return strategy;
+    }
+
+    strategy = {
+      id: this.strategies_.length + 1, name: name, strategy: fnStrategy
+    };
+
+    this.strategies_.push(strategy);
+    return strategy;
+  }
+}
+
+/**
+ * A plan restricts a strategy to a given feature's price and range of events.
+ */
+export class Plans {
+
+  constructor() {
+    this.plans_ = [];
+  }
+
+  get(name) {
+    return this.plans_.find(p => p.name === name);
+  }
+
+  addOrUpdate(name, strategyName, priceName, begin, end) {
+
+    let plan = this.get(name);
+
+    if (plan) {
+      plan.name = name;
+      plan.strategy = strategyName;
+      plan.price = priceName;
+      plan.begin = begin;
+      plan.end = end;
+      return plan;
+    }
+
+    plan = {
+      id: this.plans_.length + 1,
+      name: name,
+      strategy: strategyName,
+      price: priceName,
+      begin: !begin ? new Date().getTime() : begin,
+      end: end
+    };
+
+    this.plans_.push(plan);
+    return plan;
+  }
+
+  sunset(name, end) {
+
+    const plan = this.get(name);
+
+    if (plan && !plan.end) {
+      plan.end = !end ? new Date().getTime() : end;
+    }
+  }
+}
+
+/**
+ * The plans subscribed by a given customer.
+ */
+export class CustomerSchedule {
+
+  constructor(customerEmail) {
+    this.customer_ = customerEmail;
+    this.plans_ = [];
+  }
+
+  customer() {
+    return this.customer_;
+  }
+
+  plans() {
+    const timestamp = new Date().getTime();
+    return this.plans_.filter(p => timestamp >= p.begin && (!p.end || timestamp <= p.end));
+  }
+
+  add(planName, begin, end) {
+
+    let plan = this.plans_.find(p => p.name === planName);
+
+    if (plan) {
+      return plan;
+    }
+
+    plan = {
+      id: this.plans_.length + 1, plan: planName, begin: !begin ? new Date().getTime() : begin, end: end
+    };
+
+    this.plans_.push(plan);
+    return plan;
+  }
+
+  sunset(planName, end) {
+
+    const plan = this.plans_.find(p => p.name === planName && !p.end);
+
+    if (plan && !plan.end) {
+      plan.end = !end ? new Date().getTime() : end;
+    }
+  }
+}
+
+/**
+ * A list of app-generated events.
+ */
+export class Events {
+
+  constructor() {
+    this.events_ = [];
+  }
+
+  get(customer, feature) {
+    return this.events_.filter(e => e.customer === customer && e.feature === feature);
+  }
+
+  add(customerEmail, featureName, amount) {
+
+    const event = {
+      id: this.events_.length + 1,
+      timestamp: new Date().getTime(),
+      customer: customerEmail,
+      feature: featureName,
+      amount: amount
+    };
+
+    this.events_.push(event);
+    return event;
+  }
+}
+
+export class Pricer {
+
+  constructor(customers, features, prices, strategies, plans, schedules, events) {
+    this.customers_ = customers;
+    this.features_ = features;
+    this.prices_ = prices;
+    this.strategies_ = strategies;
+    this.plans_ = plans;
+    this.schedules_ = schedules;
+    this.events_ = events;
+  }
+
+  price(customerEmail, plans) {
+    return this.schedules_.filter(schedule => schedule.customer() === customerEmail)
+    .flatMap(schedule => {
+      return schedule.plans().filter(p => !plans || plans.indexOf(p.plan) >= 0).map(p => {
+
+        const plan = this.plans_.get(p.plan);
+        const strategy = this.strategies_.get(plan.strategy);
+        const price = this.prices_.get(plan.price);
+        const begin = plan.begin;
+        const end = plan.end;
+        const events = this.events_.get(customerEmail, price.feature).filter(
+            e => (!begin || e.timestamp >= begin) && (!end || e.timestamp <= end));
+
+        return strategy.strategy(events, price.price);
+      });
+    }).reduce((prev, cur) => prev + cur, 0);
+  }
+}
